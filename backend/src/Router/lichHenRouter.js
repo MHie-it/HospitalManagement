3b9bbb96393320d4c896b65ca7e8ace262216efc
MHie-it <leminhhiep.co@gmail.com>
2025-12-17 15:27:27 +0700

    Admin

--- backend/src/Router/lichHenRouter.js
+++ backend/src/Router/lichHenRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getLichHenByUserId, createLichHen, getLichHenByDoctorId, updateLichHen } from '../Controller/lichHenController.js';
+import { getLichHenByUserId, createLichHen, getLichHenByDoctorId, updateLichHen, getAllLichHen } from '../Controller/lichHenController.js';
 
 const router = express.Router();
 
+router.get('/all', getAllLichHen);
 router.get('/user/:userId', getLichHenByUserId);
 router.get('/doctor/:doctorId', getLichHenByDoctorId);
 router.post('/', createLichHen);
